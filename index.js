d917a009be5171363debc40fb15463c988265a2f
MevanAhmed <mevanovic@gmail.com>
2022-08-09 12:50:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+const main = document.getElementById("main");
+main.remove();
+
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = 'Mevan is the champion';
